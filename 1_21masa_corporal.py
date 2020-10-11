ff8e8bbfcf93f0d3f6a05dd6b0de57798b2f263c
rpinedat <rpineda_t@yahoo.com>
2020-10-10 20:34:04 -0500

    masa corporal

--- 1_21masa_corporal.py
+++ 1_21masa_corporal.py
@@ -1,26 +1,19 @@
 # Devuelve la masa corporal y el riesgo de enfemedades coronarias
 # Rocio Pineda
-while True:
-    try:
-        estatura = float(input("Ingrese su estatura en mts: "))
-        peso = float(input("Ingrese su peso en kg: "))
-        edad = int(input("Ingrese su edad: "))
-    except ValueError:
-        print("Debes escribir un número")
-        continue
-    else:
-        est_2 = float(pow(estatura, 2))
-        mc ,res = divmod(peso,est_2)
 
-        print("Su masa corporal es: ", mc)
+estatura = float(input("Ingrese su estatura en mts: "))
+peso = float(input("Ingrese su peso en kg: "))
+edad = int(input("Ingrese su edad: "))
 
-        if edad < 45 and mc < 22:
-            print("Su resgo coronario e bajo")
-        if edad < 45 and mc >= 22:
-            print("Su riesgo es medio")
-        if edad >= 45 and mc < 22:
-            print("Su riesgo es medio")
-        if edad >= 45 and mc >= 22:
-            print("Su riego es alto")
+mc, res = divmod(peso, pow(estatura, 2))
 
-    break
+print("Su masa corporal es: ", mc)
+
+if edad < 45 and mc < 22:
+    print("Su riesgo coronario es bajo")
+if edad < 45 and mc >= 22:
+    print("Su riesgo es medio")
+if edad >= 45 and mc < 22:
+    print("Su riesgo es medio")
+if edad >= 45 and mc >= 22:
+    print("Su riesgo es alto")
