48f0864c520312a6063c1592c2cf3aad9b5914d6
rpinedat <rpineda_t@yahoo.com>
2020-10-10 20:13:59 -0500

    1_9

--- 1_9que_nota_necesito.py
+++ 1_9que_nota_necesito.py
@@ -7,7 +7,6 @@
 nota_1 = int(input("ingrese nota de certamen 1: "))
 nota_2 = int(input("ingrese nota de certamen 2: "))
 nota_lab = int(input("ingrese nota de laboratorio: "))
-nota_min = float
 nota_prom = ((nota_1+nota_2)/3)  # Nota promedio de 3 calif
 nota_final = ((nota_prom*.7)+(nota_lab*.3))  # Nota final
 if nota_final >= 60:
