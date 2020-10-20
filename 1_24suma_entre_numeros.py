08e8f6e460d5412d664411d7caac1c2f5b1834b0
rpinedat <rpineda_t@yahoo.com>
2020-10-20 13:19:13 -0500

    20_Oct

--- 1_24suma_entre_numeros.py
+++ 1_24suma_entre_numeros.py
@@ -2,9 +2,17 @@
 
 num_1 = int(input("ingrese num 1: "))
 num_2 = int(input("ingrese num 2: "))
-
+menor = int()
+mayor = int()
 res = int()
-res2 = int()
-for i in range(num_1, num_2):
-    res2 = i + res2
-print("Res2: ", res2 - num_1)
+
+if num_1 >= num_2:
+    mayor = num_1
+    menor = num_2
+else:
+    mayor = num_2
+    menor = num_1
+
+for i in range(menor, mayor):
+    res = i + res
+print("Res: ", res - menor)
--- 1_26divisores.py
+++ 1_26divisores.py
@@ -1,17 +1,13 @@
 # Entrega todos los divisores del número ingresado
 
-num = input("Ingrese un número entero: ")
-if num.isdigit():
-    num = int(num)
-    i = 1
-    while i <= num:
-        cociente, residuo = divmod(num, i)
-        if residuo == 0:
-            print(" ", i, end="")
-            i = i+1
-        else:
-            print(end="")
-            i = i + 1
-else:
-    print("No es un número válido")
+num = int(input("Ingrese un número entero: "))
 
+i = 1
+while i <= num:
+    cociente, residuo = divmod(num, i)
+    if residuo == 0:
+        print(" ", i, end="")
+        i = i+1
+    else:
+        print(end="")
+        i = i + 1
