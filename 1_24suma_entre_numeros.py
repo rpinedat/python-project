d8cfe75f7fe0c6afb3d6390208ea47b2a4afde76
rpinedat <rpineda_t@yahoo.com>
2020-10-19 21:51:44 -0500

    correccion_19_oct

--- 1_24suma_entre_numeros.py
+++ 1_24suma_entre_numeros.py
@@ -1,17 +1,10 @@
 # Se ingresan dos núms y el resultado es la suma de los que estan entre ellos
 
-num_1 = input("ingrese num 1: ")
-num_2 = input("ingrese num 2: ")
+num_1 = int(input("ingrese num 1: "))
+num_2 = int(input("ingrese num 2: "))
 
-if num_1.isdigit() and num_2.isdigit():
-    num_1 = int(num_1)
-    num_2 = int(num_2)
-    y = num_1 + 1
-    res = y
-    while y < (num_2-1):
-        y = y + 1
-        res = res + y
-
-    print("La suma es:", res)
-else:
-    print("No es un dígito válido")
+res = int()
+res2 = int()
+for i in range(num_1, num_2):
+    res2 = i + res2
+print("Res2: ", res2 - num_1)
